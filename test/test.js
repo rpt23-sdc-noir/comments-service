a225bd2739d0382eb4cb1a59b3057b1bcc52a8ae
Amit Bhogle <abhogle17@gmail.com>
2020-11-17 22:37:54 -0700

    Updated test to handled dynamic seeding of data

--- test/test.js
+++ test/test.js
@@ -27,14 +27,14 @@ describe('/GET comments', () => {
 });
 
 describe('/GET comment', () => {
-  it('it should GET one comment for song_id 1', (done) => {
+  it('it should GET some comment for song_id 1', (done) => {
     chai
       .request(app)
       .get('/comments/1')
       .end((err, res) => {
         res.should.have.status(200);
         res.body.data.should.be.a('array');
-        res.body.data.length.should.equal(1);
+        res.body.data.length.should.be.greaterThan(0);
         done();
       });
   });
