273b1fa79401de59a2d9869e39d4d0da4cb82f68
Amit Bhogle <abhogle17@gmail.com>
2020-12-03 20:31:11 -0700

    Removed comments that are not required

--- seed/seed.js
+++ seed/seed.js
@@ -46,8 +46,5 @@ const transformOpts = { objectMode: true };
 const json2csv = new Transform(opts, transformOpts);
 input.pipe(json2csv).pipe(output);
 output.on('finish', () => {
-  console.log('seeded');
-  // eslint-disable-next-line max-len
-  // shell.exec('mongoimport --type csv -d fec-soundcloud-comments -c comments --headerline --drop /Users/abhogle/Documents/RPT23/SDC/comments-service/seed/comments.csv');
-  // mongoose.disconnect();
+  console.log('Data generated!');
 });
